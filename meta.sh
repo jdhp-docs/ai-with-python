3e8fee9dd9e3ac6bafea7c19ddc46eeb4cd66ddd
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-01-04 13:04:52 +0100

    Set metadata.

--- meta.sh
+++ meta.sh
@@ -4,17 +4,17 @@ AUTHOR_WEB_SITE="www.jdhp.org"
 
 COPYRIGHT_NOTICE="Copyright (c) 2016,2017 ${AUTHOR_NAME} (${AUTHOR_WEB_SITE})"
 
-DOCUMENT_DATE="January 1, 2017"            # TODO
-INSTITUTE="CEA Saclay - Irfu/SAp"          # TODO
-DOCUMENT_SUBJECT="TODO"                    # TODO
-DOCUMENT_KEYWORDS="TODO,TODO"              # TODO
+DOCUMENT_DATE="January 4, 2017"
+INSTITUTE=""
+DOCUMENT_SUBJECT="Python frameworks for Artificial Intelligence"
+DOCUMENT_KEYWORDS="artificial intelligence,python,framework,library,tensorflow,scipy,sklearn,pylearn"
 
-DOCUMENT_TITLE="TODO"                      # TODO
-DOCUMENT_SUBTITLE="TODO"                   # TODO
-DOCUMENT_FILE_BASE_NAME="TODO_en"          # TODO
+DOCUMENT_TITLE="Python frameworks for Artificial Intelligence"
+DOCUMENT_SUBTITLE="An overview of the main libraries"
+DOCUMENT_FILE_BASE_NAME="ai_with_python_en"
 
-DOCUMENT_GITHUB_ACCOUNT="jdhp-docs"        # TODO
-DOCUMENT_GITHUB_REPOSITORY_NAME="TODO"     # TODO
+DOCUMENT_GITHUB_ACCOUNT="jdhp-docs"
+DOCUMENT_GITHUB_REPOSITORY_NAME="ai-with-python"
 
 DOCUMENT_GITHUB_URL="https://github.com/${DOCUMENT_GITHUB_ACCOUNT}/${DOCUMENT_GITHUB_REPOSITORY_NAME}"
 DOCUMENT_ISSUE_TRACKER_URL="https://github.com/${DOCUMENT_GITHUB_ACCOUNT}/${DOCUMENT_GITHUB_REPOSITORY_NAME}/issues"
